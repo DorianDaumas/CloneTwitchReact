8dd557a7b7b578ebe504a807c91eb81550634b3a
DorianDaumas <38719906+DorianDaumas@users.noreply.github.com>
2020-06-08 22:12:58 +0200

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -3,10 +3,10 @@ import axios from "axios"
 let api = axios.create({
     headers:{
         'Client-ID': 'dqpw7npy8xuvxnjlhp674xypdou87b',
-        'Authorization: Bearer c3b4x7j8jagzi7bccjmrn0p15twuin'
+//         'Authorization: Bearer c3b4x7j8jagzi7bccjmrn0p15twuin'
 //         'Authorization:' 'Bearer c3b4x7j8jagzi7bccjmrn0p15twuin',
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
 //         'token_type': 'bearer'
 }
 
